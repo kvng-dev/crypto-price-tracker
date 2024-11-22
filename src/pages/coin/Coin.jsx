3cf3776cdbce5af32a960aa4afd76c0e657688e5
kvng-dev <oghuanlan@gmail.com>
2024-11-22 18:16:17 +0100

    fix bug

--- src/pages/coin/Coin.jsx
+++ src/pages/coin/Coin.jsx
@@ -48,46 +48,43 @@ const Coin = () => {
     fetchHistory();
   }, [currency]);
 
-  if (!coinData && !historicalData)
+  if (coinData && historicalData) {
     return (
-      <div className="spinner">
-        <div className="spin"></div>
-      </div>
-    );
-  return (
-    <div className="coin">
-      <div className="coin-name">
-        <img src={coinData.image.large} alt={coinData.name} />
-        <p>
-          <b>
-            {coinData.name} ({coinData.symbol})
-          </b>
-        </p>
-      </div>
-      <div className="coin-chart">
-        <LineChart historicalData={historicalData} />
-      </div>
+      <div className="coin">
+        <div className="coin-name">
+          <img src={coinData.image.large} alt="" />
+          <p>
+            <b>
+              {coinData.name} ({coinData.symbol})
+            </b>
+          </p>
+        </div>
+        <div className="coin-chart">
+          <LineChart historicalData={historicalData} />
+        </div>
 
-      <div className="coin-info">
-        <ul>
-          <li>Crypto Market Rank</li>
-          <li>{coinData.market_cap_rank}</li>
-        </ul>
-        <ul>
-          <li>Current Price</li>
-          <li>
-            {currency.symbol}
-            {coinData.market_data.current_price[currency.name].toLocaleString()}
-          </li>
-        </ul>
-        <ul>
-          <li>Market Cap</li>
-          <li>
-            {currency.symbol}
-            {coinData.market_data.market_cap[currency.name].toLocaleString()}
-          </li>
-        </ul>
-        {/* <ul>
+        <div className="coin-info">
+          <ul>
+            <li>Crypto Market Rank</li>
+            <li>{coinData.market_cap_rank}</li>
+          </ul>
+          <ul>
+            <li>Current Price</li>
+            <li>
+              {currency.symbol}
+              {coinData.market_data.current_price[
+                currency.name
+              ].toLocaleString()}
+            </li>
+          </ul>
+          <ul>
+            <li>Market Cap</li>
+            <li>
+              {currency.symbol}
+              {coinData.market_data.market_cap[currency.name].toLocaleString()}
+            </li>
+          </ul>
+          {/* <ul>
           <li>24 Hour High</li>
           <li>
             {currency.symbol}
@@ -105,8 +102,15 @@ const Coin = () => {
             ].toLocaleString()}
           </li>
         </ul> */}
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="spinner">
+        <div className="spin"></div>
+      </div>
+    );
+  }
 };
 export default Coin;
